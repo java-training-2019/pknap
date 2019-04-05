0d6db7598fba9f9722a43e579fffb6565a5a1fad
pknap <knapix@gmail.com>
2019-04-05 16:29:22 +0200

    Homework to do.

--- src/main/java/com/luxoft/ak47/TradeEventController.java
+++ src/main/java/com/luxoft/ak47/TradeEventController.java
@@ -1,16 +1,19 @@
 package com.luxoft.ak47;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.awt.*;
 
 @RestController
 public class TradeEventController {
 
-    @RequestMapping(value = "/tradeEvent",  produces = MediaType.TEXT_XML_VALUE)
-    String tradeEvent() {
-        return  "<tradeEvent><version>0</version></tradeEvent>";
+    @RequestMapping(value = "/tradeEvent/{id}",  produces = MediaType.TEXT_XML_VALUE)
+    String tradeEvent(@PathVariable String id) {
+        return  "<tradeEvent><id>"+ id + "</id><version>0</version></tradeEvent>";
+        //add HKG only for OBS trades
     }
 }
--- src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
+++ src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
@@ -1,10 +1,13 @@
 package com.luxoft.ak47;
 
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.hasXPath;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +17,7 @@ class TradeEventControllerTest {
     void tradeEvent() {
         //Unit test below commented:
         //new TradeEventController().tradeEvent();
-                when().get( "/tradeEvent")
+                when().get( "/tradeEvent/x")   //to fix
                 .then().statusCode(200);
     }
 
@@ -25,8 +28,24 @@ class TradeEventControllerTest {
 
     @Test
     void  shouldReturnTradesWithVersion0() {
-        when().get("/tradeEvent").then()
+        String id = "sampleId";
+        when().get("/tradeEvent/{id}", id).then()
                 .statusCode(200)
-                .body("tradeEvent.version", equalTo("0"));
+                .body("tradeEvent.version", equalTo("0"))
+                .body("tradeEvent.id", equalTo(id))
+                .body(not(hasXPath("tradeEvent/tradeLocation")));
     }
+
+    @Test
+    void shouldHaveTradeLocationFromObs() {
+        when().get("/tradeEvent/OBS-123")
+                .then()
+                .body("tradeEvent.tradeLocation", equalTo("HKG"));
+
+        //homework
+        //fix this test
+    }
+
+    //one more test that currency has uppercase 3 characters
+    //controller should return random currency from a list
 }
