3130eb838a42528c895929717e4d2578196a0dba
pknap <knapix@gmail.com>
2019-04-11 10:44:24 +0200

    Homework done.

--- src/main/java/com/luxoft/ak47/TradeEventController.java
+++ src/main/java/com/luxoft/ak47/TradeEventController.java
@@ -6,16 +6,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
-import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @RestController
 public class TradeEventController {
 
     @RequestMapping(value = "/tradeEvent/{id}",  produces = MediaType.TEXT_XML_VALUE)
     String tradeEvent(@PathVariable String id) {
-        return  "<tradeEvent><id>"+ id + "</id><version>0</version></tradeEvent>";
+        String tradeLocationTag;
+        if (id.toLowerCase().startsWith("obs") ) {
+            tradeLocationTag = "<tradeLocation>HKG</tradeLocation>";
+        } else {
+            tradeLocationTag = "";
+        }
+        return  "<tradeEvent><id>"+ id + "</id><version>0</version>" + tradeLocationTag +"<currency>" + getRandomCurrency() + "</currency></tradeEvent>";
         //add HKG only for OBS trades
     }
 
-    // add private method to generate random currency from the list d
+    // add private method to generate random currency from the list dc
+    private String getRandomCurrency() {
+        List<String> currencies = Arrays.asList("USD", "EUR", "HKG", "PLN");
+        return currencies.get(new Random().nextInt(currencies.size()));
+    }
+
+
 }
--- src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
+++ src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
@@ -1,7 +1,9 @@
 package com.luxoft.ak47;
 
 import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
+import org.hamcrest.text.CharSequenceLength;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,6 +11,7 @@ import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.hasXPath;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.text.CharSequenceLength.hasLength;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TradeEventControllerTest {
@@ -33,7 +36,7 @@ class TradeEventControllerTest {
                 .statusCode(200)
                 .body("tradeEvent.version", equalTo("0"))
                 .body("tradeEvent.id", equalTo(id))
-                .body(not(hasXPath("tradeEvent/tradeLocation")));
+                .body(not(hasXPath("/tradeEvent/tradeLocation")));
     }
 
     @Test
@@ -44,6 +47,29 @@ class TradeEventControllerTest {
 
         //homework
         //fix this test
+    }
+
+    @Test
+    void shouldNotHaveTradeLocationFromOtherThanObs() {
+        when().get("/tradeEvent/MOR-555")
+                .then()
+                .body(not(hasXPath("/tradeEvent/tradeLocation")));
+    }
+
+
+    @Test
+    void shouldHaveThreeCharactersForCurrency() {
+         when().get("/tradeEvent/MUREX-333")
+                .then()
+                .body("/tradeEvent.currency", hasLength(3));
+    }
+    @Test
+    void shouldHaveThreeUppercasedCharactersForCurrency() {
+        String currency = when().get("/tradeEvent/MUREX-333")
+                .then()
+                .extract().xmlPath().getString("/tradeEvent.currency");
+        Assertions.assertThat(currency).isUpperCase().hasSize(3);
+
     }
 
     //one more test that currency has uppercase 3 characters
